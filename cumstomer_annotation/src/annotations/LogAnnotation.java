4f61cf26f797e3605290ae844e02ff2b1bb38d2a
nolanhuang <nolanhuang@skyscanner.net>
2018-12-12 12:24:16 +0800

    init annotation demo

---
+++ cumstomer_annotation/src/annotations/LogAnnotation.java
@@ -0,0 +1,13 @@
+package annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * description:
+ *
+ * @author zhun.huang 2018-12-11
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogAnnotation {
+}
