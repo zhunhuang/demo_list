36b197736f919fe73810b2dc1927b85c9e365cff
nolanhuang <nolanhuang@skyscanner.net>
2018-12-12 20:13:08 +0800

    add

--- annotationLearn/src/helloAnnotation/annotations/LogAnnotation.java
+++ annotationLearn/src/helloAnnotation/annotations/LogAnnotation.java
@@ -1,7 +1,9 @@
-package annotations;
+package helloAnnotation.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * description:
@@ -9,5 +11,10 @@ import java.lang.annotation.RetentionPolicy;
  * @author zhun.huang 2018-12-11
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface LogAnnotation {
+    /**
+     * @return 日志级别
+     */
+    int level() default 1;
 }
